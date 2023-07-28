ec452cc1e8ffe4bfa0ab4c5029bd6b9e3ad6532f
shihabsaleem <shihabchikkushihab@gmail.com>
2023-07-28 16:13:07 +0530

    router dom

--- src/App.js
+++ src/App.js
@@ -1,5 +1,19 @@
+import Home from "./Pages/Home";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <Routes>
+          <Route path="#" element={<Home />} />
+          <Route path="#about" element={<Home />} />
+          <Route path="#portfolio" element={<Home />} />
+          <Route path="#contact" element={<Home />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
 }
 
 export default App;
