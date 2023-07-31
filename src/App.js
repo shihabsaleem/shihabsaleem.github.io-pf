e32a985646b5d00ae6e816ee9b2c0948e75c9c7e
shihabsaleem <shihabchikkushihab@gmail.com>
2023-07-31 19:02:38 +0530

    socials

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
-
 import "./App.css";
 import Header from "./Components/Header";
-
+import Home from "./Components/Home";
 
 function App() {
   return (
-   <Header/>
+    <>
+      <Header />
+      <main className="main">
+        <Home />
+      </main>
+    </>
   );
 }
 
