92913c46662802e1495ebd59d2b83b15e3d11015
shihabsaleem <shihabchikkushihab@gmail.com>
2023-08-01 14:32:42 +0530

    about

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import "./App.css";
+import About from "./Components/About";
 import Header from "./Components/Header";
 import Home from "./Components/Home";
 
@@ -8,6 +9,7 @@ function App() {
       <Header />
       <main className="main">
         <Home />
+        <About />
       </main>
     </>
   );
