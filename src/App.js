223a470371c3c6e1500c25134290461e4c0acfba
shihabsaleem <shihabchikkushihab@gmail.com>
2023-08-02 20:39:21 +0530

    footer

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import "./App.css";
 import About from "./Components/About";
 import Contact from "./Components/Contact";
 import Education from "./Components/Education";
+import Footer from "./Components/Footer";
 import Header from "./Components/Header";
 import Home from "./Components/Home";
 import Skill from "./Components/Skills";
@@ -16,6 +17,7 @@ function App() {
         <Education />
         <Skill />
         <Contact />
+        <Footer />
       </main>
     </>
   );
